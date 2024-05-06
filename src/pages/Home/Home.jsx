95d68fb1e83916a9741f6e195019cdd9d8ae807e
Emon Dev180 <shuvroemon.cse@gmail.com>
2024-05-06 07:53:10 +0600

    first commit

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import Banner from "../../Components/Banner/Banner";
+import Category from "../../Components/Category/Category";
+
+
+const Home = () => {
+    return (
+        <div>
+            <Banner></Banner>
+            <Category></Category>
+        </div>
+    );
+};
+
+export default Home;
